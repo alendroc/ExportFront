bed52baafceb8f987cd6a65a5f59f4fa9449e056
Alejandro <alejandro6chaves@gmail.com>
2025-03-11 18:04:15 -0600

    avance de riego y fixed somenthings

--- src/pages/Modulo de riego/asignarProductos.jsx
+++ src/pages/Modulo de riego/asignarProductos.jsx
@@ -1,7 +1,60 @@
+import styled from "styled-components";
+import Select, { selectClasses } from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import { BsCaretDownFill } from "react-icons/bs";
+
+
 export function AsignarProducto() { 
     return (
-        <div>
-            <h1>Asignar Producto</h1>
-        </div>
+        <Container>
+            <div>
+            <div className="flex justify-between bg-white  p-2 rounded-md gap-11 text-sm shadow-sm mb-3">
+              <h3>Temporada</h3>
+              <h3>Departamento</h3>
+            </div>
+            <div className="flex  rounded-sm gap-11 text-sm">
+            <Select
+      placeholder="Seleccione un labor"
+      indicator={<BsCaretDownFill />}
+      sx={{
+        
+        [`& .${selectClasses.indicator}`]: {
+          transition: '0.2s',
+          [`&.${selectClasses.expanded}`]: {
+            transform: 'rotate(-180deg)',
+          },
+        },
+      }}
+    >
+      <Option value="cat">Riego y drenaje</Option>
+      <Option value="fish">Fish</Option>
+      <Option value="bird">Bird</Option>
+    </Select>
+
+    <Select
+      placeholder="# Siembra"
+      indicator={<BsCaretDownFill />}
+      sx={{
+       width: '8rem',
+        [`& .${selectClasses.indicator}`]: {
+          transition: '0.2s',
+          [`&.${selectClasses.expanded}`]: {
+            transform: 'rotate(-180deg)',
+          },
+        },
+      }}
+    >
+      <Option value="dog">TODAS</Option>
+      <Option value="cat">Primera</Option>
+      <Option value="fish">Segunda</Option>
+      
+    </Select>
+            </div>
+    </div>
+        </Container>
+    
     );
  }
+ const Container = styled.div`
+ display: flex;
+ gap: 20px;`;
--- src/pages/Riego.jsx
+++ src/pages/Riego.jsx
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
 export function Riego() {
     return (
-    <Container>
+    <Container >
       <Outlet/>
     </Container>);
   }
   const Container =styled.div`
- 
+  display: flex;
+  flex-direction: column; /* Asegúrate de que el contenido se apile */
+  padding: 14px;
   `
